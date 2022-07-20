7876b2d8fbe7b5361d81438e62a20fb7906c30a6
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-07-20 23:13:48 +0530

    Update Lightsoff.cpp

--- Lightsoff.cpp
+++ Lightsoff.cpp
@@ -1,8 +1,11 @@
 /*
 Problem Statement:
-There are N+1 lights. Lights are placed at (0, 0), (1, 0), (2, 0) ... (N, 0). Initially all the lights are on. You want to turn off all of them one after one. You want to follow a special pattern in turning off the lights.
+There are N+1 lights. Lights are placed at (0, 0), (1, 0), (2, 0) ... (N, 0). Initially all the lights are on. You want to turn off all of them one after one.
+You want to follow a special pattern in turning off the lights.
 
-You will start at (0, 0). First, you walk to the right most light that is on, turn it off. Then you walk to the left most light that is on, turn it off. Then again to the right most light that is on and so on. You will stop after turning off all lights. You want to know how much distance you walked in the process. Note that distance between (a,0) and (b,0) is |a-b|.
+You will start at (0, 0). First, you walk to the right most light that is on, turn it off. 
+Then you walk to the left most light that is on, turn it off. Then again to the right most light that is on and so on. 
+You will stop after turning off all lights. You want to know how much distance you walked in the process. Note that distance between (a,0) and (b,0) is |a-b|.
 
 
 Input:
@@ -30,6 +33,37 @@ Output
 5
 
 Sample I/O Explanation:
-Testcase #2 You are initially at (0, 0) Right most on-light is (2, 0). Distance = 2. Now you are at (2, 0). Left most on-light is (0, 0). Distance = 2. Now you are at (0, 0) Right most on-light is (1, 0). Distance = 1. Now you are at (1, 0) and all lights are turned off. Total distance walked = 5.
+Testcase #2 You are initially at (0, 0) Right most on-light is (2, 0). Distance = 2. Now you are at (2, 0). Left most on-light is (0, 0). Distance = 2. 
+Now you are at (0, 0) Right most on-light is (1, 0). Distance = 1. Now you are at (1, 0) and all lights are turned off. Total distance walked = 5.
 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void lightsoff(int n){
+    int sum=0,i=0;
+    sum=n+n;
+    while(i<n){
+        n--;
+        sum=sum+(n-i);
+        //cout<<" "<<sum;
+        i++;
+        if(i<n){
+        sum=sum+((n-i)-i);
+        //cout<<" "<<sum;
+        }
+    }
+    cout<<"Total distance travelled is: "<<sum<<endl;
+}
+int main(){
+    int n;
+    cout<<"Enter the number of street lights from origin: ";
+    cin>>n;
+    lightsoff(n);
+    return 0;
+}
+
+/*
+Enter the number of street lights from origin: 1
+Total distance travelled is: 2
 */
