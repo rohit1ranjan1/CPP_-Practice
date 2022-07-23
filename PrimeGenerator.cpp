dc4fd79906fc79e3188e926027dbcf53a58a5e97
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-07-23 22:19:02 +0530

    Create PrimeGenerator.cpp

---
+++ PrimeGenerator.cpp
@@ -0,0 +1,36 @@
+/*
+Prime Generator
+Problem Statement:
+Snowden wants to generate some prime numbers for his crypto-system. Help him please! Your task is to generate all prime numbers between two given numbers.
+
+Input:
+The first line contains t, the number of test cases . Followed by t lines which contain two numbers m and n separated by a space.
+
+Output:
+For every test case print all prime numbers p such that m <= p <= n, one number per line. Separate the answers for each test case by an empty line.
+
+Constraints:
+t <= 10
+1 <= m <= n <= 1000000000
+n-m<=100000
+
+
+Time Limit:
+1 sec
+
+Example 1:
+Input
+2
+1 10
+3 5
+
+
+Output
+2
+3
+5
+7
+
+3
+5
+*/
