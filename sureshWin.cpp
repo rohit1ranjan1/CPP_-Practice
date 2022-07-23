e046e86e8972553047f64bf6e79167c94437119d
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-07-23 23:45:28 +0530

    Create sureshWin.cpp

---
+++ sureshWin.cpp
@@ -0,0 +1,36 @@
+/*
+Win
+Problem Statement:
+A bag initially contains R red balls and G green balls. Ramesh and Suresh plays this game. Each player draws a ball alternatively and never put it back. 
+The person who is the first to draw a red balls wins. Suresh always draws first. If there are no more balls in the bag and nobody has drawn a red ball, Suresh wins.
+
+What is the probability of the Suresh winning?
+
+
+Input:
+The first line will contain the number of test cases T.
+The first line of each test case will contain a number R (number of red balls) and G(number of green balls )
+
+Output:
+For each testcase, print the desired probability.
+Constraints:
+1<=T<=10
+0<=R,G<=10^3
+
+Time Limit:
+1 sec
+
+Example 1:
+Input
+3
+2 1
+1 1
+10 0
+
+
+
+Output
+0.666667
+0.500000
+1.000000
+*/
