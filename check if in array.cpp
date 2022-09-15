2c8584f02ed6e32a5fd801431f9c3ccf71342424
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-09-15 17:18:17 +0530

    Create check if in array.cpp

---
+++ check if in array.cpp
@@ -0,0 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int ifinList(int q, int* arr,int n){
+    
+    for(int i=0;i<n;i++){
+        if(q==arr[i]){
+            cout<<"YES!"<<endl;
+            return 0;
+        }
+        else{
+            cout<<"NO!"<<endl;
+            cout<<arr[i+1];
+            return 0;
+        }
+    }
+    return 0;
+}
+int main() {
+    // Write C++ code here
+    //cout << "Hello world!";
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int u=0;u<n;u++){
+        cin>>arr[u];
+    }
+    cout<<"Input done!"<<endl;
+    
+    int q;
+    cin>>q;
+    
+    ifinList(q,arr,n);
+
+    return 0;
+}
