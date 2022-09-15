eecd0907f89bb795cc3f496e0bbabd168dcd5a19
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-09-15 17:54:56 +0530

    Update check if in array.cpp

--- check if in array.cpp
+++ check if in array.cpp
@@ -4,25 +4,26 @@
 #include <iostream>
 using namespace std;
 int ifinList(int q, int* arr,int n){
-    int flag=0;
-    for(int i=0;i<n;i++){
-        cout<<arr[i];
-    }
-    /*
+    
     for(int i=0;i<n;i++){
         if(q==arr[i]){
             cout<<"YES!"<<endl;
             return 0;
         }
-        flag=1;
-        else if(q!=arr[i] && flag==0){
+    }
+    for(int i=0;i<n;i++){
+        if(q<arr[1]){
             cout<<"NO!"<<endl;
-            cout<<arr[i+1];
+            cout<<arr[0];
             return 0;
         }
-        flag=0;
+        if(q<arr[i]){
+            cout<<"NO!"<<endl;
+            cout<<arr[i-1];
+            return 0;
+        }
     }
-    */
+    
     return 0;
 }
 int main() {
@@ -53,6 +54,17 @@ int main() {
 5
 Input done!
 5
-NO!
+YES!
+
+
+5
+1
 2
+3
+4
+5
+Array Taken!
+0
+NO!
+1
 */
