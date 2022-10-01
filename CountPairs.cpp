82305ba99a5e82b4d87c631e0775c8f1de817b9e
Rohit Ranjan <83030734+rohit1ranjan1@users.noreply.github.com>
2022-10-01 19:21:45 +0530

    Create CountPairs.cpp

---
+++ CountPairs.cpp
@@ -0,0 +1,32 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int countPairs(vector<int>& A, int X){
+    int n=A.size();
+    int pairs=0;
+    int x,y,i,j;
+    for(i=0;i<n-1;i++){
+        for(j=1;j<n;j++){
+            if(i<j && A[i]==A[j]){
+            x=i+1;
+            y=j+1;
+            if((x*y)%X==0){
+                pairs++;
+                cout<<x<<" "<<y<<endl;
+            }
+            }
+        }
+    }
+    return pairs;
+}
+int main() {
+    vector<int> A{3,3,2,3,2,3,3};
+    int pairs=countPairs(A,3);
+    cout<<pairs<<endl;
+
+    return 0;
+}
+/*
+
+*/
